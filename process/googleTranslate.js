8334fdde369129085d7ea7b6356650ab207b54b2
tucobenedicto <phkubiak@gmail.com>
2019-01-29 00:32:08 -0500

    authenticating google cloud credentials via env variables

--- process/googleTranslate.js
+++ process/googleTranslate.js
@@ -2,6 +2,10 @@ const { Translate } = require('@google-cloud/translate');
 
 const translate = new Translate({
   projectId: process.env.GOOGLE_PROJECT_ID,
+  credentials: {
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  },
 });
 
 module.exports = (translationInputText, translationOutputLanguage) => (
