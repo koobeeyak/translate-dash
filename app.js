a90710fe677606a0f9a113e6265267d6e4ff05a2
tucobenedicto <phkubiak@gmail.com>
2019-01-29 00:02:36 -0500

    postbuild specific to heroku to buil our client

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var translationRouter = require('./routes/translation');
 
 var app = express();
@@ -18,9 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use('/', indexRouter);
 app.use('/api/translations', translationRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
